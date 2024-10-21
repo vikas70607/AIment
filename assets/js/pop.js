53cea3833e9c5b5f4163a145b0ae1d1cd70e3510
VIKAS <vikas70607@gmail.com>
2024-10-22 00:47:42 +0530

    Added pop ups

---
+++ assets/js/pop.js
@@ -0,0 +1,14 @@
+setTimeout(function() {
+    document.getElementById('popup-card').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+
+    document.body.style.overflow = 'hidden';
+}, 15000); // 15 seconds delay
+
+// Function to close popup
+function closePopup() {
+    document.getElementById('popup-card').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+
+    document.body.style.overflow = 'auto';
+}
