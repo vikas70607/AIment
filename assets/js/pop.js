a58afa8e865ef7070c173d7ad9cb0f064345a67b
VIKAS <vikas70607@gmail.com>
2024-10-25 00:05:19 +0530

    Finished Pop-Up

--- assets/js/pop.js
+++ assets/js/pop.js
@@ -3,7 +3,7 @@ setTimeout(function() {
     document.getElementById('overlay').style.display = 'block';
 
     document.body.style.overflow = 'hidden';
-}, 5000); // 15 seconds delay
+}, 10000); // 15 seconds delay
 
 // Function to close popup
 function closePopup() {
