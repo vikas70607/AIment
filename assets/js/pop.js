67e27e80987831835f2fcb304560144622a629c8
SyedEhsan06 <ctech1699@gmail.com>
2025-01-28 14:29:55 +0530

    url fixed and PopUp saved in session

--- assets/js/pop.js
+++ assets/js/pop.js
@@ -1,14 +1,18 @@
-setTimeout(function() {
-    document.getElementById('popup-card').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
+// Check if the popup has already been shown in this session
+if (!sessionStorage.getItem('popupShown')) {
+    setTimeout(function() {
+        document.getElementById('popup-card').style.display = 'block';
+        document.getElementById('overlay').style.display = 'block';
+        document.body.style.overflow = 'hidden';
 
-    document.body.style.overflow = 'hidden';
-}, 10000); // 15 seconds delay
+        // Set a flag in sessionStorage to indicate the popup has been shown
+        sessionStorage.setItem('popupShown', 'true');
+    }, 5000); // 5 seconds delay
+}
 
 // Function to close popup
 function closePopup() {
     document.getElementById('popup-card').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
-
     document.body.style.overflow = 'auto';
 }
